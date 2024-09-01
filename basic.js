b8f2775589ca639fa07f489200170088d5e931ec
Rhenz Largo <rhenzlargo80@gmail.com>
2024-09-01 22:15:54 +0800

    Added age question

--- basic.js
+++ basic.js
@@ -7,11 +7,19 @@ const rl = readline.createInterface({
 
 console.log("Testing!");
 
-function introduction(name) {
+function introduction(age,name) {
     console.log(`Hello ${name}!`);
+    console.log(`You are ${age} years old!`);
 }
 
 rl.question("Enter your name: ", (inputtedName) => {
     introduction(inputtedName);
     rl.close(); 
 })
+
+rl.question("Enter your name: ", (inputtedName) => {
+    rl.question("Enter your age: ", (inputtedAge) => {       
+        introduction(inputtedName, inputtedAge);
+        rl.close(); 
+    });
+});
